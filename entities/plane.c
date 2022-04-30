4b40380e74743c519fbc56729084adc57b9a0d3c
Matheus Tavares <matheus.bernardino@usp.br>
2022-04-30 09:33:28 -0300

    Allow plane to be shaded from the back face too
    That is, rays hitting the back face will now be considered for
    intersection with the plane.

--- entities/plane.c
+++ entities/plane.c
@@ -6,7 +6,7 @@ int ray_intersects_plane(struct ray *r, struct entity *e, struct intersection *i
 	struct plane *p = &e->u.p;
 
 	float divisor = vec3_dot(r->dir, p->normal);
-	if (divisor >= 0)
+	if (!divisor)
 		return 0;
 	float dist = vec3_dot(vec3_sub(p->p0, r->pos), p->normal) / divisor;
 	if (dist < 0)
